aa0b750f7181192b07316e2cc9303fb496c96d35
Sara Stephens <sara@sarastephens.com>
2013-07-02 14:12:24 +0100

    Solved problem

--- prob/code.js
+++ prob/code.js
@@ -1,18 +1,39 @@
-function isPrimeNumber(number) { 
-    
-    if(number == 2)
-    return true;
-    
+function isPrimeNumber(number) {
+
+    if (number == 2 || number % 2 == 0)
+        return true;
+
     for (i = 2; i < number; i++) {
 
         if (number % i == 0)
             return false;
     }
-        return true;
+    return true;
+}
+
+function isDivisableByNumber(number, divideByNumber) {
+
+    return number % divideByNumber == 0;
+}
+
+function findFactors(number, array) {
+
+    if (number == 1)
+        return array;
+
+    for (i = 2; i <= number; i++) {
+        
+        if (isDivisableByNumber(number, i)) {
+        
+            if (isPrimeNumber(i)) {
+        
+                array.push(i);
+                findFactors(number / i, array);
+                break;
+             }
+        }      
+
     }
+}
 
-    function isDivisableByNumber(number, divideByNumber) {
 
-        return number % divideByNumber == 0;
-
-    } 
--- prob/specs.js
+++ prob/specs.js
@@ -55,3 +55,35 @@ it("should return false given the number 9 is divisable by 5", function () {
 });
 
 });
+
+describe("Get prime factors", function () {
+
+    it("should return 2,2,3 when given the number 12", function () {
+
+        var result = [];
+
+        findFactors(12, result);
+        expect(result[0]).toBe(2);
+        expect(result[1]).toBe(2);
+        expect(result[2]).toBe(3);
+
+    });
+
+    it("should return 5,7,13,29 when given the number 13195", function () {
+
+        var result = [];
+        findFactors(13195, result);
+        expect(result[0]).toBe(5);
+        expect(result[1]).toBe(7);
+        expect(result[2]).toBe(13);
+        expect(result[3]).toBe(29);
+    });
+
+    it("should return the highest factors when given the number 600851475143", function () {
+
+        var result = [];
+        findFactors(600851475143, result);
+        console.info(result[result.length-1]);
+    });
+});
+
